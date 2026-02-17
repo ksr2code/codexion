04a1463c86e9e425e5d2f77bc8f2e11094e963dd
ksr2code <ksr2code@gmail.com>
2026-02-17 15:00:55 +0100

    feat: updated init functions added init for all resources and cleaning in seperate file

---
+++ coders/clean.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   clean.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ksmailov <ksmailov@student.42heilbronn.de  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/17 14:47:34 by ksmailov          #+#    #+#             */
+/*   Updated: 2026/02/17 14:47:36 by ksmailov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "codexion.h"
+
+static void	destroy_dongles(t_sim *sim)
+{
+	int	i;
+
+	i = -1;
+	while (++i < sim->num_coders)
+	{
+		pthread_mutex_destroy(&sim->dongles[i].mutex);
+		pthread_cond_destroy(&sim->dongles[i].cond);
+	}
+	free(sim->dongles);
+}
+
+static void	destroy_coders(t_sim *sim)
+{
+	free(sim->coders);
+}
+
+void	destroy_simulation(t_sim *sim)
+{
+	if (sim->coders)
+	{
+		destroy_dongles(sim);
+		destroy_coders(sim);
+	}
+	pthread_mutex_destroy(&sim->log_mutex);
+	free(sim);
+}
--- coders/init.c
+++ coders/init.c
@@ -6,7 +6,7 @@
 /*   By: ksmailov <ksmailov@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/17 13:43:05 by ksmailov          #+#    #+#             */
-/*   Updated: 2026/02/17 14:11:10 by ksmailov         ###   ########.fr       */
+/*   Updated: 2026/02/17 14:49:19 by ksmailov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,52 @@ t_sim	*init_simulation(t_config *cfg)
 	return (sim);
 }
 
-void	destroy_simulation(t_sim *sim)
+static int	init_dongles(t_sim *sim, t_config *cfg)
 {
-	pthread_mutex_destroy(&sim->log_mutex);
-	free(sim);
+	int	i;
+
+	sim->dongles = malloc(cfg->number_of_coders * sizeof(t_dongle));
+	if (!sim->dongles)
+		return (0);
+	i = -1;
+	while (++i < cfg->number_of_coders)
+	{
+		sim->dongles[i].id = i;
+		sim->dongles[i].available = 1;
+		sim->dongles[i].cooldown_until = 0;
+		pthread_mutex_init(&sim->dongles[i].mutex, NULL);
+		pthread_cond_init(&sim->dongles[i].cond, NULL);
+	}
+	return (1);
+}
+
+static int	init_coders(t_sim *sim, t_config *cfg)
+{
+	int	i;
+
+	sim->coders = malloc(cfg->number_of_coders * sizeof(t_coder));
+	if (!sim->coders)
+		return (0);
+	i = -1;
+	while (++i < cfg->number_of_coders)
+	{
+		sim->coders[i].id = i + 1;
+		sim->coders[i].left_dongle = &sim->dongles[i];
+		sim->coders[i].right_dongle = &sim->dongles[(i + 1)
+			% cfg->number_of_coders];
+		sim->coders[i].cfg = cfg;
+		sim->coders[i].sim = sim;
+		sim->coders[i].compiles_done = 0;
+		sim->coders[i].last_compile_start = sim->start_time;
+		sim->coders[i].alive = 1;
+	}
+	return (1);
+}
+
+int	init_resources(t_sim *sim, t_config *cfg)
+{
+	sim->num_coders = cfg->number_of_coders;
+	if (!init_dongles(sim, cfg) || !init_coders(sim, cfg))
+		return (0);
+	return (1);
 }
